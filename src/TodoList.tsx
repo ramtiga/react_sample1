4af16dc9d34d374d40a76ecbd9755f399d01972c
ramtiga <dhanegm731@gmail.com>
2026-06-16 20:03:46 +0900

    todoに完了チェックボックス追加

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,34 +1,67 @@
 import { useState } from "react"
 
+type Todo = {
+  id: number
+  text: string
+  completed: boolean
+}
+
 function TodoList() {
-  const [todos, setTodos] = useState<string[]>([])
+  const [todos, setTodos] = useState<Todo[]>([])
   const [input, setInput] = useState("")
 
   const handlerInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value)
   }
 
+  // Todo追加
   const addTodo = () => {
     if (input.trim()) {
-      setTodos((prev) => [...prev, input])
+      setTodos((prev) => [...prev,
+        {
+          id: Date.now(),
+          text: input,
+          completed: false
+        }])
       setInput("")
     }
   }
 
-  const createRemoveHandler = (index: number) => () => {
-    setTodos((prev) => prev.filter((_, i) => i !== index))
+  // 完了未完了切り替え
+  const toggleTodo = (id: number) => {
+    setTodos(prev =>
+      prev.map(todo =>
+        todo.id == id ? { ...todo, completed: !todo.completed} : todo
+      )
+    )
+  }
+
+  // Todo削除
+  const removeTodo = (id: number) => () => {
+    setTodos((prev) => prev.filter(todo => todo.id !== id))
   }
 
   return (
     <div>
       <input value={input} onChange={handlerInputChange} />
       <button onClick={addTodo}>追加</button>
-      {todos.map((todo, index) => (
-        <li key={index}>
-          {todo}
-          <button onClick={createRemoveHandler(index)}>削除</button>
-        </li>
-      ))}
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <label style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <input type='checkbox' checked={todo.completed} onChange={() => { toggleTodo(todo.id) }} />
+              <span style={{
+                textDecoration: todo.completed ? 'line-through' : 'none',
+                color: todo.completed ? '#999' : 'inherit'
+              }}>
+            {todo.text}
+            </span>
+            </label>
+            <button onClick={removeTodo(todo.id)}>削除</button>
+          </li>
+        ))}
+
+      </ul>
     </div>
   )
 }
